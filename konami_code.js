8e5c7d708b07410cb95e126b3868a79bc533b9bf
jacobsilver2 <jacobsilver2@mac.com>
2018-05-10 09:09:58 -0400

    dfdf

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-const doc = document.querySelector('body');
-doc.addEventListener('keydown', init(e));
 
-function init(e) {
-  const key = parseInt(e.detail || e.which);
+function init() {
+  const doc = document.querySelector('body');
+  doc.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.location);
   
-  if (key === code[index]) {
-    index++;
+    if (key === code[index]) {
+      index++;
+    }
+    
+    if (index === code.length) {
+      alert("Hurray!");
+      index = 0;
+    } else {
+      index = 0;
+    }
+
+    });
+
+  
   }
-  
-  if (index === code.length) {
-    alert("Hurray!");
-    index = 0;
-  } else {
-    index = 0;
-  }
-  
-}
