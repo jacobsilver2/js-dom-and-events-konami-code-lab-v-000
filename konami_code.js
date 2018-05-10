639db636ffb6e4cd8a7cc7d5f2ebd406f3bbb552
jacobsilver2 <jacobsilver2@mac.com>
2018-05-10 09:15:01 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,15 +4,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.location);
+    const key = parseInt(e.which || e.detail);
   
     if (key === code[index]) {
       index++;
-    }
     
-    if (index === code.length) {
-      alert("Hurray!");
-      index = 0;
+    
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
     } else {
       index = 0;
     }
@@ -21,3 +22,5 @@ function init() {
 
   
   }
+
+
